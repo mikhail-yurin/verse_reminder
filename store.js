bdff8b90a947a8144f6c5b05c30fd8ca256417f9
Михаил Юрин <MYurin@alfabank.ru>
2022-05-27 13:18:41 +0300

    UI improved

--- store.js
+++ store.js
@@ -1,6 +1,7 @@
 const storeKey = 'verse_reminder_calendar_url';
 const input = document.getElementById('target_url');
 const button = document.getElementById('submit');
+const list = document.getElementById('list');
 
 if (input) {
   chrome.storage.sync.get([storeKey], function (result) {
@@ -9,6 +10,23 @@ if (input) {
     }
   });
 }
+
+chrome.alarms.getAll().then((alerts) => {
+  if (list) {
+    const uniqueAlerts = [
+      ...new Map(
+        alerts
+          .map((alert) => ({ ...alert, name: alert.name.replace(/\s?\[.*\].*/, '') }))
+          .map((item) => [item['name'], item])
+      ).values()];
+
+    uniqueAlerts.forEach(({ name, scheduledTime }) => {
+      const li = document.createElement('li');
+      li.innerText = `${name} ${new Date(scheduledTime)}`;
+      list.appendChild(li);
+    });
+  }
+}).catch((err) => { });
 
 button.onclick = () => {
   chrome.storage.sync.set({ [storeKey]: input.value?.replace(/https?:\/\//, '') }, function () {
